41fc8038364166746a4cb548cea51e8afdc26d5f
Chema <106692495+ElChema-Nc@users.noreply.github.com>
2023-05-30 19:14:41 -0600

    Add files via upload

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 } catch {
-var update = execSync('git remote set-url origin https://github.com/GataNina-Li/GataBot-MD.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/ElChema-Nc/Shark-Bot.git && git pull')
 await m.reply(update.toString())
 }}
 handler.help = ['update']
