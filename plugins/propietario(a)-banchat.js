fbffb77c0a1185661db86e4c7edc1f9031a9d9b9
Chema <106692495+ElChema-Nc@users.noreply.github.com>
2023-09-26 18:37:03 +0200

    Add files via upload

--- plugins/propietario(a)-banchat.js
+++ plugins/propietario(a)-banchat.js
@@ -18,7 +18,7 @@ m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘽𝘼𝙉𝙀�
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = /^banchat|bangata|banchat2$/i
+handler.command = /^banchat|banshark|banchat2$/i
 handler.botAdmin = true
 handler.admin = true 
 export default handler
