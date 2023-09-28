3cd72403aa3e24c050af76ae0b360d41f964afe0
Chema <106692495+ElChema-Nc@users.noreply.github.com>
2023-09-28 20:24:45 +0200

    Update lista-usuarios_baneados.js

--- plugins/lista-usuarios_baneados.js
+++ plugins/lista-usuarios_baneados.js
@@ -6,7 +6,7 @@ let caption = `
 │
 │ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Usuarios Baneados' : conn.getName(jid)}
 │ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
-│ *Estos usuarios no puedes Usar a GataBot*
+│ *Estos usuarios no puedes Usar a SharkBot*
 *╰•·–––––––––––––––––––·•*`.trim()
 
 await conn.reply(m.chat, caption, m, { mentions: await conn.parseMention(caption) })}
