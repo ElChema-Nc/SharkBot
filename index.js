5c33880dd5c9788d61455d230e5e0121e84583e1
Chema <106692495+ElChema-Nc@users.noreply.github.com>
2023-09-29 17:25:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Chema\nShark\nBot', {
+say('Shark\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
